498d26afd9feea5286d12f0e93d576352849aa0b
Jonathan Vasquez <jonathan@jvasquez.net>
2018-07-15 14:56:13 -0400

    First Commit with new classes. Still need to set default project

---
+++ ClassLibrary1/Organization.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassLibrary1
+{
+    public class Organization
+    {
+        string orgType { get; set; }
+        string orgName{ get; set; }
+        string orgAddress1 { get; set; }
+        string orgAddress2 { get; set; }
+        string orgCity { get; set; }
+        string orgState { get; set; }
+        string orgZipCode { get; set; }
+        string orgPhone { get; set; }
+        private string _orgTwitter;
+        public string orgTwitter
+        {
+            //Ensure Twitter address starts with @
+            get
+            {
+                return _orgTwitter;
+            }
+
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _orgTwitter = value;
+                }
+                else
+                {
+                    throw new Exception("The Twitter address ust beging with @");
+                }
+            }
+
+
+        }
+    }
+}
